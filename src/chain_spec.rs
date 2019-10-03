1f945c79cc89f357c70a81a2e4f6ac118c3f766c
Joshy Orndorff <admin@joshyorndorff.com>
2019-10-02 22:08:47 -0800

    Grab runtime and config from recipes through Cargo.toml.

--- src/chain_spec.rs
+++ src/chain_spec.rs
@@ -1,8 +1,6 @@
 use primitives::{Pair, Public};
-use node_template_runtime::{
-	AccountId, BabeConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
-	SudoConfig, IndicesConfig, SystemConfig, WASM_BINARY, 
-};
+use runtime::{AccountId, GenesisConfig};
+use runtime_genesis::testnet_genesis;
 use babe_primitives::{AuthorityId as BabeId};
 use grandpa_primitives::{AuthorityId as GrandpaId};
 use substrate_service;
@@ -71,7 +69,7 @@ impl Alternative {
 				|| testnet_genesis(vec![
 					get_authority_keys_from_seed("Alice"),
 					get_authority_keys_from_seed("Bob"),
-				], 
+				],
 				get_from_seed::<AccountId>("Alice"),
 				vec![
 					get_from_seed::<AccountId>("Alice"),
@@ -103,33 +101,5 @@ impl Alternative {
 			"" | "local" => Some(Alternative::LocalTestnet),
 			_ => None,
 		}
-	}
-}
-
-fn testnet_genesis(initial_authorities: Vec<(AccountId, AccountId, GrandpaId, BabeId)>,
-	root_key: AccountId, 
-	endowed_accounts: Vec<AccountId>,
-	_enable_println: bool) -> GenesisConfig {
-	GenesisConfig {
-		system: Some(SystemConfig {
-			code: WASM_BINARY.to_vec(),
-			changes_trie_config: Default::default(),
-		}),
-		indices: Some(IndicesConfig {
-			ids: endowed_accounts.clone(),
-		}),
-		balances: Some(BalancesConfig {
-			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
-			vesting: vec![],
-		}),
-		sudo: Some(SudoConfig {
-			key: root_key,
-		}),
-		babe: Some(BabeConfig {
-			authorities: initial_authorities.iter().map(|x| (x.3.clone(), 1)).collect(),
-		}),
-		grandpa: Some(GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.2.clone(), 1)).collect(),
-		}),
 	}
 }
